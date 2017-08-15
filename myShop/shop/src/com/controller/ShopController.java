34f8ed773b0af0f5a1423a8eb6868b93cc4d851a
ibtissemAbs <31000048+ibtissemAbs@users.noreply.github.com>
2017-08-15 07:49:51 +0100

    Add files via upload

---
+++ myShop/shop/src/com/controller/ShopController.java
@@ -0,0 +1,68 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.model.Product;
+
+@Controller
+public class ShopController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String redirect(Model model) {
+        return "redirect:/products";
+    }
+	
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+    public String produits(Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		List<Product> prods = restTemplate.getForObject("http://localhost:3000/products", List.class);
+        model.addAttribute("products", prods);
+        model.addAttribute("activetab", "all");
+        return "shop";
+    }
+	
+	
+	@RequestMapping(value = "/iphones", method = RequestMethod.GET)
+    public String iphones(Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		List<Product> prods = restTemplate.getForObject("http://localhost:3000/products?type=iphones", List.class);
+        model.addAttribute("products", prods);
+        model.addAttribute("activetab", "iphones");
+        return "shop";
+    }
+	
+	@RequestMapping(value = "/smartphones", method = RequestMethod.GET)
+    public String smartphones(Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		List<Product> prods = restTemplate.getForObject("http://localhost:3000/products?type=smartphones", List.class);
+        model.addAttribute("products", prods);
+        model.addAttribute("activetab", "smartphones");
+        return "shop";
+    }
+	
+	@RequestMapping(value = "/tablettes", method = RequestMethod.GET)
+    public String tablettes(Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		List<Product> prods = restTemplate.getForObject("http://localhost:3000/products?type=tablettes", List.class);
+        model.addAttribute("products", prods);
+        model.addAttribute("activetab", "tablettes");
+        return "shop";
+    }
+	
+	@RequestMapping(value = "/details", method = RequestMethod.GET)
+    public String detail(@RequestParam(value="id", required=false, defaultValue="0") String id, Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		Product prod = restTemplate.getForObject("http://localhost:3000/products/"+id, Product.class);
+		model.addAttribute("phone", prod);
+        return "detail";
+    }
+	
+}
---
+++ myShop/shop/src/com/model/Product.java
@@ -0,0 +1,76 @@
+package com.model;
+
+public class Product {
+	
+	private Integer id;
+	private String url;
+	private String name;
+	private String brand;
+    private Double price;
+    private String description;
+    private Boolean isAvailable;
+    
+    
+    
+	public Product() {
+		
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Boolean getIsAvailable() {
+		return isAvailable;
+	}
+	public void setIsAvailable(Boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+	
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
+	public String toString() {
+		return "product [name=" + name + ", brand=" + brand + ", price="
+				+ price + ", isAvailable=" + isAvailable + "]";
+	}
+    
+    
+}
